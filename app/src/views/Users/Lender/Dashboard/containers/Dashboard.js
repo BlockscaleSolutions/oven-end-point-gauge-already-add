03a3d7d2eeec05dfc8130e696cf6bbb46dcc9358
Adam J Lemmon <adamjlemmon@gmail.com>
2018-06-19 05:55:57 -0400

    debt contract

--- app/src/views/Users/Lender/Dashboard/containers/Dashboard.js
+++ app/src/views/Users/Lender/Dashboard/containers/Dashboard.js
@@ -2,6 +2,8 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router";
 import * as actions from "../../../../../_actions/loans";
 import Dashboard from "../components/Dashboard";
+// import web3 from "Web3";
+
 
 const mapStateToProps = state => ({
     open: state.Loans.loans,
--- app/truffle/contracts/Debt.sol
+++ app/truffle/contracts/Debt.sol
@@ -2,21 +2,36 @@ pragma solidity ^0.4.23;
 
 contract Debt {
 
+    mapping(address => Loan) loans_;
+
+    event LoanCreated(uint256 amount, address borrower, address lender);
+    event LoanSignOff(address signedOff);
+    event LoanCompleted(bool obligationMet);
+
     struct Loan {
         uint256 amount;
         address borrower;
         address lender;
-        mapping (address => bool) hasSignedOff;
+        bool loanReceived;
+        bool loanPaid;
     }
 
-    function createLoanRequest () public {
-
+    function createLoanRequest (
+        uint256 amount,
+        address borrower,
+        address lender
+    )   external {
+        loans_[borrower] = Loan(amount, borrower, lender, false);
     }
 
-    function acceptLoanRequest () public {}
+    function loanReceived () public {
+        // assume no loans for 0 eth
+        require(loans_[msg.sender].amount > 0);
+        loans[msg.sender].loanReceived = true;
+    }
 
-    function fillLoanRequest () public {}
-
-    function loanPaidOff () public {}
-        
+    function loanPaid (address borrower) public {
+        require(loans_[borrower].lender == msg.sender);
+        loans[msg.sender].loanPaid = true;
+    }
 }
