6c744ee81399a8ca69a297f5d82384ded6a67d78
Adam J Lemmon <adamjlemmon@gmail.com>
2018-06-19 21:19:10 -0400

    move truffle dir and add indexed params

--- app/src/views/Users/Lender/Dashboard/truffle/contracts/Debt.sol
+++ app/src/views/Users/Lender/Dashboard/truffle/contracts/Debt.sol
@@ -1,14 +1,11 @@
 pragma solidity ^0.4.23;
 
 contract Debt {
-
-    string public test = "ADAM";
-
     mapping(address => Loan) public loans_;
 
-    event LoanCreated(uint256 amount, address borrower, address lender);
-    event LoanReceived(address borrower);
-    event LoanPaid(address lender);
+    event LoanCreated(uint256 amount, address indexed borrower, address indexed lender);
+    event LoanReceived(address indexed lender, address indexed borrower);
+    event LoanPaid(address indexed lender, address indexed borrower);
 
     struct Loan {
         uint256 amount;
@@ -34,13 +31,13 @@ contract Debt {
         // assume no loans for 0 eth
         require(loans_[msg.sender].amount > 0);
         loans_[msg.sender].loanReceived = true;
-        emit LoanReceived(msg.sender);
+        emit LoanReceived(loans_[msg.sender].lender, msg.sender);
     }
 
     function loanPaid (address borrower) public {
         require(loans_[borrower].lender == msg.sender);
         loans_[msg.sender].loanPaid = true;
-        emit LoanPaid(borrower);
+        emit LoanPaid(msg.sender, borrower);
         delete loans_[msg.sender];
     }
 }
